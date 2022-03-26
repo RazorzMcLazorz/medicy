a5ee08390cfeffb377a436e590a432f1043c0562
RazorzMcLazorz <nateyoungdev@gmail.com>
2022-03-26 02:14:27 -0600

    styles updated, mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css';
 
-function App() {
+import MainPage from './components/MainPage/mainPage'
+
+const App = () => {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<div />} />
-        </Routes>
-      </BrowserRouter>
-  );
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/MainPage/mainPage.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div className='MainPage'>
+      {/* <Background /> */}
+      <div className='body'>
+        <h1>Medicy</h1>
+        {/* {this.props.db ? (
+          <div className='buttonRow'>
+            {this.props.user ? (
+              <div>
+                <a className='button' href='/create'>
+                  New
+                </a>
+                <a className='button'>Load</a>
+              </div>
+            ) : (
+              <a className='button' href='/login'>
+                Login
+              </a>
+            )}
+          </div>
+        ) : (
+          <div className='buttonRow'>Loading for Database to boot up please wait a minute! =)</div>
+        )} */}
+      </div>
+    </div>
+  )
+}
+
+export default MainPage
