43b63ccb6d35d600e414d529f326b9932072facf
RazorzMcLazorz <nateyoungdev@gmail.com>
2022-03-27 23:13:33 -0600

    background img fix

--- src/common/mainPageBackground/mainPageBackground.tsx
+++ src/common/mainPageBackground/mainPageBackground.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
 import './styles.css'
 
-const MainPageBackground = () => {
-  return (
-    <img
-      alt='Main Page'
-      src='./assets/mainMenuBackGround.jpg'
-      className='mainPageBackgroundImage'
-    />
-  )
-}
+const MainPageBackground = () => (
+  <img alt='Main Page' src='./assets/mainMenuBackGround.jpg' className='mainPageBackgroundImage' />
+)
 
 export default MainPageBackground
