583c10dedcbaac3af5cee82863a0ffb9d48d536f
RazorzMcLazorz <nateyoungdev@gmail.com>
2022-08-26 10:11:43 -0600

    BASETIEL

--- src/configs/gameConfig.ts
+++ src/configs/gameConfig.ts
@@ -1,13 +1,24 @@
 import CONFIG, { DIFFICULTY, TIME_LIMIT } from './createGameConfig'
+import { TILE } from './tileConfig'
+
+export interface IMap {
+  xCoord: number
+  yCoord: number
+  // This is the default tile before a user builds on the tile it self
+  baseTile: TILE
+}
 
 export interface IDefaultGameConfig {
   mapSize: number
   difficulty: DIFFICULTY
   timeLimit: TIME_LIMIT
+  map: IMap[][]
 }
 
 export const DEFAULT_GAME_CONFIG: IDefaultGameConfig = {
   mapSize: CONFIG.MAP_EXACT_SIZE[CONFIG.DEFAULT_MAP_SIZE],
   difficulty: CONFIG.DEFAULT_DIFFICULTY,
   timeLimit: CONFIG.DEFAULT_HAS_TIME_LIMIT,
+  // This is the map the user will use all the time
+  map: [],
 }
